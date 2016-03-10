7d5ebfb15b04402cd37e86f8093a5768f1dc6414
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-03-10 11:52:44 +0100

    applied ipfinfo api,styles update,html update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by seyi adeleke on 3/8/2016.
  */
+$(document).ready(function(){
+    //api for city and country
+    var url = 'http://ipinfo.io/json'
+    $.getJSON(url)
+        .done (function (location) {
+            $('#country').html(location.country);
+            $('#city').html(location.city);
+            $('#latitude').html(location.latitude);
+            $('#longitude').html(location.longitude);
+
+        });
+});
