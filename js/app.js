86e87429b5b8f4ad4171ee89823f0410ce5555ae
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-03-10 13:14:22 +0100

    city api change,created locationcoords variable

--- js/app.js
+++ js/app.js
@@ -3,12 +3,20 @@
  */
 $(document).ready(function(){
     //api for city and country
-    var url = 'http://ipinfo.io/json'
-    $.getJSON(url)
-        .done (function (location) {
+    var cityUrl = 'http://ip-api.com/json';
+    var weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=Lagos,Ng&appid=44db6a862fba0b067b1930da0d769e98';
+    $.getJSON(cityUrl).done (function (location) {
             $('#country').html(location.country);
             $('#city').html(location.city);
-            $('#location').html(location.loc);
+            var locationCoords = document.getElementById("location");
+           locationCoords.innerHTML = "lat:"+ location.lat+ ",";
+           locationCoords.innerHTML += "long:" + location.lon;
+            $.getJSON(openWeatherMap, {
+            lat: location.lat,
+            lon: location.lon,
+            units: 'metric',
+            APPID: APIKEY
+        })
 
-        });
+    });
 });
