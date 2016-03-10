837bd208f05f51a3e53a4e539473dcc11203d864
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-03-10 14:23:47 +0100

    fetch temperature,html update

--- js/app.js
+++ js/app.js
@@ -4,19 +4,21 @@
 $(document).ready(function(){
     //api for city and country
     var cityUrl = 'http://ip-api.com/json';
-    var weatherApi = 'http://api.openweathermap.org/data/2.5/weather?q=Lagos,Ng&appid=44db6a862fba0b067b1930da0d769e98';
-    $.getJSON(cityUrl).done (function (location) {
-            $('#country').html(location.country);
-            $('#city').html(location.city);
-            var locationCoords = document.getElementById("location");
-           locationCoords.innerHTML = "lat:"+ location.lat+ ",";
-           locationCoords.innerHTML += "long:" + location.lon;
-            $.getJSON(openWeatherMap, {
-            lat: location.lat,
-            lon: location.lon,
-            units: 'metric',
-            APPID: APIKEY
-        })
 
+    $.getJSON(cityUrl).done (function (location) {
+        $('#country').html(location.country);
+        $('#city').html(location.city);
+        var locationCoords = document.getElementById("location");
+        locationCoords.innerHTML = "lat:"+ location.lat+ ",";
+        locationCoords.innerHTML += "long:" + location.lon;
+        var weatherApi ='http://api.openweathermap.org/data/2.5/weather?lat='+location.lat+'&lon='+location.lon+'&appid=19037534c7624a5254a20b799495dbcc';
+
+        $.getJSON(weatherApi,{
+            lat:location.lat,
+            lon:location.lon
+        }).done(function(weather) {
+            $("#temperature").text(Math.round(weather.main.temp));
+
+        })
     });
 });
