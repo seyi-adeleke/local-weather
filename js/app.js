943b25cf50abdc18e2dbef2d0b6f3f66eb92e15e
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-03-14 14:28:33 +0100

    added current weathe from api

--- js/app.js
+++ js/app.js
@@ -9,23 +9,24 @@ $(document).ready(function(){
         $('#country').html(location.country);
         $('#city').html(location.city+",");
         var locationCoords = document.getElementById("location");
-        locationCoords.innerHTML = "LAT:"+ location.lat+ ",";
+        locationCoords.innerHTML = "LAT:"+ location.lat + ",";
         locationCoords.innerHTML += "LONG:" + location.lon;
         var weatherApi ='http://api.openweathermap.org/data/2.5/weather?lat='+location.lat+'&lon='+location.lon+'&units=metric&appid=19037534c7624a5254a20b799495dbcc';
 
         $.getJSON(weatherApi,{
             lat:location.lat,
             lon:location.lon
-        }).done(function(weather) {
-            $("#temperature").html(weather.main.temp);
+        }).done(function(weathers) {
+            $("#current_weather").html(weathers.weather[0].main);
+            $("#temperature").html(weathers.main.temp);
             $( "#button" ).click(function()
             {
-                var CelsiusTemp =weather.main.temp.toFixed(2);
+                var CelsiusTemp =weathers.main.temp.toFixed(2);
                 $("#temperature").html(CelsiusTemp);
             });
             $( "#button2" ).click(function()
             {
-                var CelsiusTemp =weather.main.temp;
+                var CelsiusTemp =weathers.main.temp;
                 var fahrenheitTemp = (CelsiusTemp  * (9/5) + 32).toFixed(2);
                 $("#temperature").html(fahrenheitTemp);
             });
