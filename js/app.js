771d1f9ba197688d7db6c910737a98afb4064017
seyi-adeleke <seyiadeleke42@yahoo.com>
2016-04-17 21:19:18 +0100

    update

--- js/app.js
+++ js/app.js
@@ -8,13 +8,14 @@ $(document).ready(function(){
     $.getJSON(cityUrl).done (function (location) {
         $('#city').html(location.city+",");
         $('#country').html(location.country);
-     /*   var locationCoords = document.getElementById("location");
-        locationCoords.innerHTML = "LAT:"+ location.lat + ",";
-        locationCoords.innerHTML += "LONG:" + location.lon;
-     */
+         /* var locationCoords = document.getElementById("location");
+         locationCoords.innerHTML = "LAT:"+ location.lat + ",";
+         locationCoords.innerHTML += "LONG:" + location.lon;
+         */
+
         var weatherApi ='http://api.openweathermap.org/data/2.5/weather?lat='+location.lat+'&lon='+location.lon+'&units=metric&appid=19037534c7624a5254a20b799495dbcc';
         $.getJSON(weatherApi).done(function(weather){
-            $("#current_weather").html(weather.weather[0].main);
+            $("#current_weather").html(weather.weather[0].description);
             $("#temperature").html(weather.main.temp);
             $( "#button" ).click(function()
             {
@@ -27,14 +28,16 @@ $(document).ready(function(){
                 var fahrenheitTemp = (CelsiusTemp  * (9/5) + 32).toFixed(2);
                 $("#temperature").html(fahrenheitTemp);
             });
-            var weather_icon = weather.weather[0].main;
-            if(weather_icon == "Clouds"){
+            var weather_icon = weather.weather[0].description;
+            if(weather_icon == "few clouds"){
                 $("#icons").addClass("wi-cloud");
             }
-            else if(weather_icon == "Rain"){
-                $("#icons").addClass("wi-rain");
+            else if(weather_icon == "light rain"){
+                $("#icons").addClass("wi-showers");
             }
-
+            else if(weather_icon == "clear sky"){
+                $("#icons").addClass("wi-day-sunny");
+            }
         });
 
     });
