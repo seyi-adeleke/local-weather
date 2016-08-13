4f082b69763d21680757a615be3bd8d2fd900ab2
adetokunbo <adelekeadetokunbo01@gmail.com>
2016-08-13 22:09:00 +0100

    code update

--- js/app.js
+++ js/app.js
@@ -16,17 +16,18 @@ $(document).ready(function(){
         var weatherApi ='http://api.openweathermap.org/data/2.5/weather?lat='+location.lat+'&lon='+location.lon+'&units=metric&appid=19037534c7624a5254a20b799495dbcc';
         $.getJSON(weatherApi).done(function(weather){
             $("#current_weather").html(weather.weather[0].description);
-            $("#temperature").html(weather.main.temp);
-            $( "#button" ).click(function()
+            $("#temperature").html(weather.main.temp + " °C");
+            $( "#button1" ).click(function()
             {
                 var CelsiusTemp=weather.main.temp.toFixed(2);
-                $("#temperature").html(CelsiusTemp);
+                $("#temperature").html(CelsiusTemp + " °C");
+
             });
-            $( "#button2" ).click(function()
+            $("#button2").click(function()
             {
-                var CelsiusTemp =weather.main.temp;
+                var CelsiusTemp =weather.main.temp.toFixed(2);
                 var fahrenheitTemp = (CelsiusTemp  * (9/5) + 32).toFixed(2);
-                $("#temperature").html(fahrenheitTemp);
+                $("#temperature").html(fahrenheitTemp +" °F" );
             });
             var weather_icon = weather.weather[0].description;
             if(weather_icon == "few clouds"){
@@ -38,9 +39,32 @@ $(document).ready(function(){
             else if(weather_icon == "clear sky"){
                 $("#icons").addClass("wi-day-sunny");
             }
+            else if(weather_icon == "broken clouds"){
+                $("#icons").addClass("wi-cloudy");
+            }
         });
 
     });
+    var buttonHighlightStyles = {
+        'border-color':'aliceblue',
+        'color':'aliceblue'
+    };
+    var buttonUnhighlightStyles = {
+        "border-color":"gray",
+        "color":"gray"
+    };
+
+
+    $('#button1').click(function(){
+        $('#button2').css(buttonUnhighlightStyles);
+        $('#button1').css(buttonHighlightStyles);
+    });
+    $('#button2').click(function(){
+        $('#button1').css(buttonUnhighlightStyles);
+        $('#button2').css(buttonHighlightStyles);
+
+    });
+
 
 
 });
